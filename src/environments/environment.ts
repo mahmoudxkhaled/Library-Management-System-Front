63ab47b51f2146b25713611597b8c6d885e3e342
Mahmoud Khaled <mahmoudxkhaled@gmail.com>
2025-03-28 13:55:31 +0200

    adding to enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,3 +7,18 @@ export const environment = {
   apiUrlWithoutAPI: 'https://localhost:7279',
   assetsPath: '../assets',
 };
+
+
+
+
+// export const environment = {
+//   production: false,
+//   appVersion: 'v8.2.4',
+//   isMockEnabled: true,
+//   apiUrl: 'https://lmsapi.mkcodespace.com/api',
+//   apiUrlWithoutAPI: 'https://lmsapi.mkcodespace.com',
+//   assetsPath: '../assets',
+// };
+
+
+
